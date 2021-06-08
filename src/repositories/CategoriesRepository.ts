060a97b88aa1b36100a69485e004d39bbf1177a8
Victor Campos <campos.viictor@gmail.com>
2021-06-08 20:10:20 -0300

    feat: add CategoriesRepository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,38 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+export class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): Category {
+    const newCategory = new Category();
+
+    Object.assign(newCategory, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(newCategory);
+
+    return newCategory;
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+}
