5c564c4dd142df7779ba3c4a866fb8e138c9bb1c
Victor Campos <campos.viictor@gmail.com>
2021-06-10 18:30:55 -0300

    refactor: add create & list categories controllers

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,16 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  const newCategory = createCategoryService.execute({ name, description });
-
-  return response.status(201).json(newCategory);
-});
+categoriesRoutes.post("/", (request, response) =>
+  createCategoryController.handle(request, response)
+);
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.status(200).json(all);
+  listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
